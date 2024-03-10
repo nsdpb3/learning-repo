7d3c42062165a228bb4bb7df97914c18004bc5cd
nsdbp3 <xramov14881@gmail.com>
2024-03-10 20:10:45 +0300

    Added topics

---
+++ OOP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'viktor/oop',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5ca30744b8d4f847abe8f99a868bd76849ae6891',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'viktor/oop' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5ca30744b8d4f847abe8f99a868bd76849ae6891',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
